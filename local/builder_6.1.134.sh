cf7c2bc82364960a9797f369741d980d453f35a7
cctv18 <85936817+cctv18@users.noreply.github.com>
2026-02-10 10:51:56 +0800

    Set ReSukiSU as default KSU branch
    Due to the slowdown on SukiSU upstream updating, we decide to use ReSukiSU (the most active fork of KSU) as default KSU choice.

--- local/builder_6.1.134.sh
+++ local/builder_6.1.134.sh
@@ -15,8 +15,8 @@ read -p "是否启用susfs？(y/n，默认：y): " APPLY_SUSFS
 APPLY_SUSFS=${APPLY_SUSFS:-y}
 read -p "是否启用 KPM？(b-(re)sukisu内置kpm, k-kernelpatch next独立kpm实现, n-关闭kpm，默认：n): " USE_PATCH_LINUX
 USE_PATCH_LINUX=${USE_PATCH_LINUX:-n}
-read -p "KSU分支版本(y=SukiSU Ultra, r=ReSukiSU, n=KernelSU Next, m=MKSU, k=KSU, l=lkm模式(无内置KSU), 默认：y): " KSU_BRANCH
-KSU_BRANCH=${KSU_BRANCH:-y}
+read -p "KSU分支版本(y=SukiSU Ultra, r=ReSukiSU, n=KernelSU Next, m=MKSU, k=KSU, l=lkm模式(无内置KSU), 默认：r): " KSU_BRANCH
+KSU_BRANCH=${KSU_BRANCH:-r}
 read -p "是否应用 lz4 1.10.0 & zstd 1.5.7 补丁？(y/n，默认：y): " APPLY_LZ4
 APPLY_LZ4=${APPLY_LZ4:-y}
 read -p "是否应用 lz4kd 补丁？(y/n，默认：n): " APPLY_LZ4KD
